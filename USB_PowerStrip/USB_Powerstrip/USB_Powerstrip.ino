d7f1ca5085856002927f6dd8f76606e5717f7668
Richard P <tuxbox.guru@gmail.com>
2018-06-01 07:01:03 +0100

    Add files via upload

--- USB_PowerStrip/USB_Powerstrip/USB_Powerstrip.ino
+++ USB_PowerStrip/USB_Powerstrip/USB_Powerstrip.ino
@@ -1,26 +1,4 @@
-/*
-  Blink
-
-  Turns an LED on for one second, then off for one second, repeatedly.
-
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino
-  model, check the Technical Specs of your board at:
-  https://www.arduino.cc/en/Main/Products
-
-  modified 8 May 2014
-  by Scott Fitzgerald
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-  modified 8 Sep 2016
-  by Colby Newman
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/Blink
-*/
+#include "MillisTimer.h"
 
 
 #define RELAY1 10
@@ -35,9 +13,12 @@
 char buffer[10];
 char bufptr=0;
 
+MillisTimer timer1 = MillisTimer(10);
+int commTimer=0;
+
+
 // the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
   Serial.begin(19200);
   pinMode(RELAY1, OUTPUT);
   pinMode(RELAY2, OUTPUT);
@@ -51,6 +32,9 @@ void setup() {
   pinMode(LED_POWER, OUTPUT);
   
   digitalWrite(LED_POWER, HIGH);
+  timer1.setInterval(10);
+  timer1.expiredHandler(resetComms);
+  timer1.start();
   
 }
 
@@ -58,15 +42,15 @@ void setup() {
 void loop() {
    char inChar;
 
+   timer1.run();
    while (Serial.available() > 0)
    {
-     if (bufptr < 10)
-      {
+     
           inChar = Serial.read();
+          commTimer=10;
           if ((inChar == 13) || (inChar == 10))
           {
 
-            
             if (bufptr < 5)
                 {
                   bufptr=0;
@@ -106,13 +90,23 @@ void loop() {
           {
             buffer[bufptr] = inChar;
             bufptr++;
-          }  
-      } else
-        bufptr=0; // reset to zero as its bigger tgan the buffer
-
-
-    
+            if (bufptr >= 10)
+               bufptr=0;
+          }      
    }
+}
 
-      // wait for a second
+void resetComms(MillisTimer &mt)
+{
+  if (commTimer>0)
+  {
+   commTimer--;
+  } else
+  {
+    if (commTimer==0)
+       bufptr=0;
+
+    commTimer=-1;
+  }
+
 }
